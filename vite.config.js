c2584dec15c5a060ba9473591c78a82c6e655229
ShawonBinRashid <shawonbinrashid@gmail.com>
2025-11-27 18:25:37 +0600

    blank page issue fixing-5

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/blood-donor-app/',
   plugins: [react()],
   server: {
     port: 3000,
     host: '0.0.0.0',
     headers: {
-      'Content-Security-Policy': "default-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: ws: wss: data: blob:;"
+      'Content-Security-Policy':
+        "default-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: ws: wss: data: blob:;"
     }
   },
   build: {
