813e8613d780ec6312b6c5414c590f372054119c
ShawonBinRashid <shawonbinrashid@gmail.com>
2025-11-27 13:18:37 +0600

    blank page check

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     port: 3000,
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: 'dist'
   }
 })
